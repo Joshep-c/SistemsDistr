ff9f9ba98dd48a7eb949772946422ac16e7d1e69
Josh <jccahuanala@unsa.edu.pe>
2025-05-19 01:43:49 -0500

    Creación del proyecto

---
+++ rmi_spring/src/main/java/com/epis/rmi_spring/RmiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.epis.rmi_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmiSpringApplication.class, args);
+	}
+
+}
---
+++ rmi_spring/src/test/java/com/epis/rmi_spring/RmiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epis.rmi_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
